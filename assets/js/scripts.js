3f1ac33a68dc441f837e9113e53bc0091188524f
Dusio <duszczor@gmail.com>
2020-03-07 19:14:40 +0100

    Refactoring #2

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,7 +13,7 @@ function scroll_to(clicked_link, nav_height) {
 
 
 jQuery(document).ready(function() {
-	
+
 	/*
 	    Navigation
 	*/
@@ -25,30 +25,30 @@ jQuery(document).ready(function() {
 	$('.top-content .text').waypoint(function() {
 		$('nav').toggleClass('navbar-no-bg');
 	});
-	
+
     /*
         Background slideshow
     */
-    $('.top-content').backstretch("assets/img/backgrounds/1.jpg");
-    $('.call-to-action-container').backstretch("assets/img/backgrounds/1.jpg");
-    $('.testimonials-container').backstretch("assets/img/backgrounds/1.jpg");
-    
+    /*$('.top-content').backstretch("assets/img/backgrounds/1.jpg");*/
+    /*$('.call-to-action-container').backstretch("assets/img/backgrounds/1.jpg");*/
+    /*$('.testimonials-container').backstretch("assets/img/backgrounds/1.jpg");*/
+
     $('#top-navbar-1').on('shown.bs.collapse', function(){
     	$('.top-content').backstretch("resize");
     });
     $('#top-navbar-1').on('hidden.bs.collapse', function(){
     	$('.top-content').backstretch("resize");
     });
-    
+
     $('a[data-toggle="tab"]').on('shown.bs.tab', function(){
     	$('.testimonials-container').backstretch("resize");
     });
-    
+
     /*
         Wow
     */
     new WOW().init();
-	
+
 });
 
 
@@ -58,6 +58,5 @@ jQuery(window).load(function() {
 		Hidden images
 	*/
 	$(".testimonial-image img").attr("style", "width: auto !important; height: auto !important;");
-	
 });
 
